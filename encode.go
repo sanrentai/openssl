89afdd8c19a767c6b2fb7557e3c693f1bad5f5c1
gouguoyin <245629560@qq.com>
2021-08-09 12:01:07 +0800

    add encode.go

---
+++ encode.go
@@ -0,0 +1,53 @@
+package openssl
+
+const (
+	BASE32 = "base32"
+	BASE64 = "base64"
+	HEX    = "hex"
+)
+
+// encode defines encode struct.
+type encode struct {
+	openssl
+}
+
+// newEncode returns a new encode instance.
+func newEncode() encode {
+	return encode{}
+}
+
+// FromString encrypts from string.
+// 对字符串进行加密
+func (e encode) FromString(s string) encode {
+	if s == "" {
+		return e
+	}
+	e.input = string2bytes(s)
+	return e
+}
+
+// FromBytes encrypts from byte slice.
+func (e encode) FromBytes(b []byte) encode {
+	if len(b) > 0 {
+		e.input = b
+	}
+	return e
+}
+
+// ToString outputs as string.
+func (e encode) ToString() string {
+	output := e.output
+	if output == nil {
+		output = e.input
+	}
+	return bytes2string(output)
+}
+
+// ToBytes outputs as byte slice.
+func (e encode) ToBytes() []byte {
+	input, output := e.input, e.output
+	if len(input) == 0 {
+		return []byte{}
+	}
+	return output
+}
